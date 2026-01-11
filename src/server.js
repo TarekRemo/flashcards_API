1947e0fb8a049ceb12cf1252f931fc5e40fd32f6
TarekRemo <taekrummo64@gmail.com>
2026-01-11 15:34:23 +0100

    adding admin routes + using logger middleware

--- src/server.js
+++ src/server.js
@@ -2,14 +2,18 @@ import express from 'express';
 import 'dotenv/config';
 import authRouter from './routers/auth_router.js';
 import flashcardsRouter from './routers/flashcards_router.js';
+import adminRouter from './routers/admin_router.js';
+import  logger  from './middlewares/logger.js';
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
+app.use(logger);
 app.use('/auth', authRouter);
 
 app.use('/flashcard', flashcardsRouter);
+app.use('/admin', adminRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
